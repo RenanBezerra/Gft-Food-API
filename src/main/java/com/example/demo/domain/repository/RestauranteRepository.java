206735d24352f94b71afbdb76f5eb079fd699f29
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-22 14:18:38 -0300

    3.16
    criando repositorio de restaurante

---
+++ src/main/java/com/example/demo/domain/repository/RestauranteRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.domain.repository;
+
+import java.util.List;
+
+import com.example.demo.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+
+	List<Restaurante> listar();
+	
+	Restaurante buscar(Long id);
+	
+	Restaurante salvar(Restaurante restaurante);
+	
+	void remover(Restaurante restaurante);
+
+	
+}
