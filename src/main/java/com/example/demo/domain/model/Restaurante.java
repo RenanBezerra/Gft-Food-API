08ad771f17cf8ce8b8ccf76ab2f2e0839cc725dd
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-22 15:05:23 -0300

    3.19
    adicionando propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/example/demo/domain/model/Restaurante.java
+++ src/main/java/com/example/demo/domain/model/Restaurante.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -24,13 +23,14 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String nome;
 
-	@Column(name = "taxa_frete")
+	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
 	@ManyToOne
-	@JoinColumn(name = "cozinha_id")
+	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 
 }
