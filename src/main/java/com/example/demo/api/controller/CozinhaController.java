9c8c435c3f1108bedf9c14308a3cbbdebcb7b98c
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-05 14:21:35 -0300

    4.13
    Implementando content negotiation para retornar JSON ou  XML, dois metodos iguais sendo chamado pela diferença do produces

--- src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,14 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
-	@GetMapping
-	public List<Cozinha> listar() {
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Cozinha> listar1() {
+		System.out.println("Listar 1");
+		return cozinhaRepository.listar();
+	}
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public List<Cozinha> listar2() {
+		System.out.println("Listar 2");
 		return cozinhaRepository.listar();
 	}
 
