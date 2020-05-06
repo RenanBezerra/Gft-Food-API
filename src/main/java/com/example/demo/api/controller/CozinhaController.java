fa0bf0bec6b4c42aa28c61e303e76a4b9854f73a
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-06 11:30:57 -0300

    4.23
    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.api.model.CozinhasXmlWrapper;
@@ -43,6 +47,12 @@ public class CozinhaController {
 		  return ResponseEntity.notFound().build();
 		
 		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 
 }
