c0ff942486a2b9ca73473deea1161a824bff5215
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-06 11:08:53 -0300

    4.21
    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -3,14 +3,11 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.api.model.CozinhasXmlWrapper;
@@ -34,18 +31,18 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
-	@ResponseStatus(HttpStatus.CREATED )
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		  Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION,"http://localhost:8080/cozinhas");
+		  if(cozinha != null) {
+			  return ResponseEntity.ok(cozinha);
+			  
+		  }
+		  
+		  return ResponseEntity.notFound().build();
 		
-		return ResponseEntity
-				.status(HttpStatus.FOUND)
-				.headers(headers)
-				.build();
+		
 	}
 
 }
