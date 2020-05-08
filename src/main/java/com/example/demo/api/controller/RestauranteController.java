90f436e4c1e042b086e78939bfd339026cfb1306
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-08 12:37:19 -0300

    4.30
    Modelando e implementando a inclusão de recursos de restaurantes, e tratando o erro  de id de cozinha na classe de serviço, passando a mensagem no response entity

--- src/main/java/com/example/demo/api/controller/RestauranteController.java
+++ src/main/java/com/example/demo/api/controller/RestauranteController.java
@@ -3,14 +3,19 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.Restaurante;
 import com.example.demo.domain.repository.RestauranteRepository;
+import com.example.demo.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -18,20 +23,37 @@ public class RestauranteController {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
+	@Autowired
+	private CadastroRestauranteService cadastroRestauranteService;
+
 	@GetMapping
-	public List<Restaurante> listar(){
+	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
 	}
-	
+
 	@GetMapping("/{RestauranteId}")
-	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId){
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
-		
+
 		if (restaurante != null) {
 			return ResponseEntity.ok(restaurante);
-			
+
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+
+		try {
+			restaurante = cadastroRestauranteService.salvar(restaurante);
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
 	}
 }
---
+++ src/main/java/com/example/demo/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
+import com.example.demo.domain.model.Cozinha;
+import com.example.demo.domain.model.Restaurante;
+import com.example.demo.domain.repository.CozinhaRepository;
+import com.example.demo.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+
+		}
+
+		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.salvar(restaurante);
+	}
+}
