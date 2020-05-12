2e816aa979c9febc24ba92ed2b54950f6fc908b9
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-12 15:05:34 -0300

    5.1
    Implementando consultas JPQL em repositórios

--- src/main/java/com/example/demo/domain/repository/CozinhaRepository.java
+++ src/main/java/com/example/demo/domain/repository/CozinhaRepository.java
@@ -2,11 +2,16 @@ package com.example.demo.domain.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.example.demo.domain.model.Cozinha;
 
+@Component
 public interface CozinhaRepository {
 
 	List<Cozinha> listar();
+	
+	List<Cozinha> consultaPorNome(String nome);
 
 	Cozinha buscar(Long id);
 
--- src/main/java/com/example/demo/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/example/demo/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -6,13 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.model.Cozinha;
 import com.example.demo.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
@@ -21,6 +21,12 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 	@Override
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	@Override
+	public List<Cozinha> consultaPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome = :nome",Cozinha.class)
+				.setParameter("nome",nome)
 				.getResultList();
 	}
 
