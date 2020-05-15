0203fd4cda9168c47174eb8b5f3300f9fdb4828a
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-15 19:05:04 -0300

    5.7
    Usando as Keywords para definir critérios de query methods

--- src/main/java/com/example/demo/api/controller/TesteController.java
+++ src/main/java/com/example/demo/api/controller/TesteController.java
@@ -1,5 +1,6 @@
 package com.example.demo.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.model.Cozinha;
+import com.example.demo.domain.model.Restaurante;
 import com.example.demo.domain.repository.CozinhaRepository;
+import com.example.demo.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -19,15 +22,30 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
 	@GetMapping("/cozinhas/por-nome")
 	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome){
-		return cozinhaRepository.findTodasByNome(nome);
+		return cozinhaRepository.findTodasByNomeContaining(nome);
 	}
 	@GetMapping("/cozinhas/unica-por-nome")
 	public Optional<Cozinha> cozinhaPorNome(String nome){
 		return cozinhaRepository.findByNome(nome);
 	}
 	
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal){
+		
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			String nome,Long cozinhaId){
+		
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
 	
 	
 	
