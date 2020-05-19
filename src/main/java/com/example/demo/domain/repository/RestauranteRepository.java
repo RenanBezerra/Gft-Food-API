bc8d5a55f75908c46b8aba5f26080a9d05e0fc86
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-18 21:34:37 -0300

    5.10
    Externalizando consultas JPQL para um arquivo XML, para query muito grandes cria-se um arquivo para inserir as queries

--- src/main/java/com/example/demo/domain/repository/RestauranteRepository.java
+++ src/main/java/com/example/demo/domain/repository/RestauranteRepository.java
@@ -16,7 +16,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial,BigDecimal taxaFinal);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id= :id")
+	//@Query("from Restaurante where nome like %:nome% and cozinha.id= :id")
 	List<Restaurante> consultaPorNome(String nome,@Param("id") Long cozinha);
 	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome,Long cozinha);
