398eee08992e3f832f8953095b90a8abc1ef6b6c
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-19 21:29:00 -0300

    5.13
    Implementando uma consulta simples com Criteria API

--- src/main/java/com/example/demo/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/example/demo/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,7 +7,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
+import org.hibernate.Criteria;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -21,31 +24,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	private EntityManager manager;
 
 	@Override
-	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome,
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-		StringBuilder jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
-
-		HashMap<String, Object> parametros = new HashMap<String, Object>();
-
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-
-		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-
-		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
+		
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
 		return query.getResultList();
 
