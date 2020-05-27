4bf35c7956d6320b5b9b09ecd5336278287f4a10
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-05-26 22:15:25 -0300

    6.7..
    criei a classe Produto e o relacionamento @ManyToOne

--- src/main/java/com/example/demo/domain/repository/CozinhaRepository.java
+++ src/main/java/com/example/demo/domain/repository/CozinhaRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.model.Cozinha;
