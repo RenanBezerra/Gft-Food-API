e5738736049d2da7e18e860026631c29c086ed29
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-06-01 22:12:49 -0300

    8.3
    Lançando exceções do tipo ResponseStatusException

--- src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.Cozinha;
 import com.example.demo.domain.repository.CozinhaRepository;
 import com.example.demo.domain.service.CadastroCozinhaService;
@@ -71,8 +73,14 @@ public class CozinhaController {
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cozinhaId) {
-
-		cadastroCozinhaService.excluir(cozinhaId);
+		try {
+			cadastroCozinhaService.excluir(cozinhaId);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+			// throw new ServerWebInputException(e.getMessage());
+		
+		}
 	}
 
 }
