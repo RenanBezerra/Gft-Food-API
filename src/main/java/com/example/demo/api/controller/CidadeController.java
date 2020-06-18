61878691e92f061d3fa32fddfed0650cb294990a
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-06-18 00:03:30 -0300

    8.13
    Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/example/demo/api/controller/CidadeController.java
+++ src/main/java/com/example/demo/api/controller/CidadeController.java
@@ -79,19 +79,6 @@ public class CidadeController {
 
 	}
 
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
-	}
-
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-
-		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
-	}
+	
 
 }
