76341fdb08abe5d694287fc77071ffbdfc4651eb
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-06-18 23:23:18 -0300

    8.14
    Implementando exception handler

--- src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.demo.domain.exception.EntidadeEmUsoException;
 import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.exception.NegocioException;
 
@@ -36,4 +37,13 @@ public class ApiExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.CONFLICT)
+				.body(problema);
+	}
+	
 }
