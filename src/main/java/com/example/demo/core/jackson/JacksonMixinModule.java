7fe47e4ed7e01f23444ad02bb0f4afecddb968a9
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-07-12 10:06:36 -0300

    11.3
    Criando classes de mixin para usar as anotações do Jackson

---
+++ src/main/java/com/example/demo/core/jackson/JacksonMixinModule.java
@@ -0,0 +1,23 @@
+package com.example.demo.core.jackson;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.api.model.mixin.RestauranteMixin;
+import com.example.demo.domain.model.Restaurante;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+@Component
+public class JacksonMixinModule extends SimpleModule{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JacksonMixinModule() {
+		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+		
+		
+	}
+	
+}
