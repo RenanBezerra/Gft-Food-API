9763610f12c875f4f826d0c590b38354bcf8b257
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-07-14 18:51:43 -0300

    11.8
    refatorando o código para usar OffsetDateTime

--- src/main/java/com/example/demo/api/exceptionhandler/Problem.java
+++ src/main/java/com/example/demo/api/exceptionhandler/Problem.java
@@ -1,6 +1,6 @@
 package com.example.demo.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -20,9 +20,9 @@ public class Problem {
 	private String detail;
 
 	private String userMessage;
-	private LocalDateTime timestamp;
+	private OffsetDateTime timestamp;
 	private List<Object> objects;
-	
+
 	@Getter
 	@Builder
 	public static class Object {
--- src/main/java/com/example/demo/domain/model/Pedido.java
+++ src/main/java/com/example/demo/domain/model/Pedido.java
@@ -1,7 +1,7 @@
 package com.example.demo.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,35 +28,35 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private BigDecimal subtotal;
 	private BigDecimal taxaFrete;
 	private BigDecimal valorTotal;
 
 	@Embedded
 	private Endereco enderecoEntrega;
-	
-	private StatusPedido status;
-	
-	@CreationTimestamp
-	private LocalDateTime dataCriacao;
 
-	private LocalDateTime dataConfirmacao;
-	private LocalDateTime dataCancelamento;
-	private LocalDateTime dataEntrega;
-	
+	private StatusPedido status;
+
+	@CreationTimestamp
+	private OffsetDateTime dataCriacao;
+
+	private OffsetDateTime dataConfirmacao;
+	private OffsetDateTime dataCancelamento;
+	private OffsetDateTime dataEntrega;
+
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private FormaPagamento formaPagamento;
-	
+
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Restaurante restaurante;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "usuario_cliente_id", nullable = false)
 	private Usuario cliente;
-	
+
 	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens = new ArrayList<>();
 
