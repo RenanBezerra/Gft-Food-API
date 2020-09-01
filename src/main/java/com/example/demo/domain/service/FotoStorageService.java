955229a02e4a4f7b7506a47cd8fa5618b38fb370
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-09-01 12:50:38 -0300

    14.10
    Implementando a remoção e substituição de arquivos de fotos no serviço de armazenagem

--- src/main/java/com/example/demo/domain/service/FotoStorageService.java
+++ src/main/java/com/example/demo/domain/service/FotoStorageService.java
@@ -3,13 +3,25 @@ package com.example.demo.domain.service;
 import java.io.InputStream;
 import java.util.UUID;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
 import lombok.Builder;
 import lombok.Getter;
 
 public interface FotoStorageService {
 
 	void armazenar(NovaFoto novaFoto);
+	
+	void remover(String nomeArquivo);
 
+	default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+		
+		if (nomeArquivoAntigo != null) {
+			this.remover(nomeArquivoAntigo);
+		}
+	}
+	
 	default String gerarNomeArquivo(String nomeOriginal) {
 		return UUID.randomUUID().toString() + "_" + nomeOriginal;
 	}
@@ -21,4 +33,6 @@ public interface FotoStorageService {
 		private String nomeArquivo;
 		private InputStream inputStream;
 	}
+
+	
 }
--- src/main/java/com/example/demo/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/example/demo/infrastructure/service/storage/LocalFotoStorageService.java
@@ -1,5 +1,6 @@
 package com.example.demo.infrastructure.service.storage;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,24 +16,37 @@ public class LocalFotoStorageService implements FotoStorageService {
 
 	@Value("${gftFood.storage.local.diretorio-fotos}")
 	private Path diretorioFotos;
-	
-	
+
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 		try {
 			Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
-			
-			FileCopyUtils.copy(novaFoto.getInputStream(), 
-					Files.newOutputStream(arquivoPath));
+
+			FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
 		} catch (Exception e) {
 			throw new StorageException("Não foi possível armazenar arquivo.", e);
 		}
 	}
-	
-	private Path getArquivoPath(String nomeArquivo) {
-		
+
+	@Override
+	public void remover(String nomeArquivo) {
+
+		Path arquivoPath = getArquivoPath(nomeArquivo);
+
 		Path arquivoFoto = Paths.get("C:\\Users\\rebg\\Desktop\\catalogo", nomeArquivo);
-		
+
+		try {
+			Files.deleteIfExists(arquivoPath);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possivel excluir arquivo.", e);
+		}
+
+	}
+
+	private Path getArquivoPath(String nomeArquivo) {
+
+		Path arquivoFoto = Paths.get("C:\\Users\\rebg\\Desktop\\catalogo", nomeArquivo);
+
 		return arquivoFoto;
 	}
 
