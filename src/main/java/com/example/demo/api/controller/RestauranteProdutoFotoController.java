af94981a982b8aa84f457d4885efd9c35234ed27
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-09-02 12:08:45 -0300

    14.13
    Servindo arquivos de fotos pela API , criei endPoint que retorna a imagem no postman  como resposta

--- src/main/java/com/example/demo/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/example/demo/api/controller/RestauranteProdutoFotoController.java
@@ -1,11 +1,14 @@
 package com.example.demo.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,10 +19,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.demo.api.assembler.FotoProdutoModelAssembler;
 import com.example.demo.api.model.FotoProdutoModel;
 import com.example.demo.api.model.input.FotoProdutoInput;
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.FotoProduto;
 import com.example.demo.domain.model.Produto;
 import com.example.demo.domain.service.CadastroProdutoService;
 import com.example.demo.domain.service.CatalogoFotoProdutoService;
+import com.example.demo.domain.service.FotoStorageService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -27,38 +32,56 @@ public class RestauranteProdutoFotoController {
 
 	@Autowired
 	private CadastroProdutoService cadastroProduto;
-	
+
 	@Autowired
 	private CatalogoFotoProdutoService catalogoFotoProduto;
-	
+
 	@Autowired
 	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
-	
+
+	@Autowired
+	private FotoStorageService fotoStorageService;
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public FotoProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+
+		return fotoProdutoModelAssembler.toModel(fotoProduto);
+	}
+
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
+	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+			@Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 		Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
-		
+
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
-		
+
 		FotoProduto foto = new FotoProduto();
 		foto.setProduto(produto);
 		foto.setDescricao(fotoProdutoInput.getDescricao());
 		foto.setContentType(arquivo.getContentType());
 		foto.setTamanho(arquivo.getSize());
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
-		
+
 		FotoProduto fotoSalva = catalogoFotoProduto.salvar(foto, arquivo.getInputStream());
-		
+
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
-	
-	@GetMapping
-	public FotoProdutoModel buscar(@PathVariable Long restauranteId, 
+
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
 			@PathVariable Long produtoId) {
-		FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
-		
-		return fotoProdutoModelAssembler.toModel(fotoProduto);
+
+		try {
+			FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+
+			InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(inputStream));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}
+
 	}
-	
+
 }
