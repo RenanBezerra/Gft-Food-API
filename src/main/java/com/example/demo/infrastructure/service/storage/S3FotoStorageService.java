5bb1ba531dc5b7771fe74d7a74df1a13118df8b3
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-09-03 17:30:42 -0300

    14.22
    Definindo bean do client da Amazon S3 e configurando credenciais

--- src/main/java/com/example/demo/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/example/demo/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,13 +2,18 @@ package com.example.demo.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.example.demo.domain.service.FotoStorageService;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService{
 
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 		
