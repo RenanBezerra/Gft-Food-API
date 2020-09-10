ff9dbfd6e9f3b93d0bf941e1bb8f1c1d4d0a786a
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-09-10 16:36:58 -0300

    15.2
    Configurando o projeto para envio de e-mails usando servidor SMTP

--- src/main/java/com/example/demo/domain/model/StatusPedido.java
+++ src/main/java/com/example/demo/domain/model/StatusPedido.java
@@ -1,28 +1,26 @@
 package com.example.demo.domain.model;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public enum StatusPedido {
 
-	CRIADO("Criado"),
-	CONFIRMADO("Confirmado", CRIADO),
-	ENTREGUE("Entregue", CONFIRMADO),
+	CRIADO("Criado"), CONFIRMADO("Confirmado", CRIADO), ENTREGUE("Entregue", CONFIRMADO),
 	CANCELADO("Cancelado", CRIADO);
 
 	private String descricao;
 	private List<StatusPedido> statusAnteriores;
-	
-	 StatusPedido(String descricao,StatusPedido... statusAnteriores){
-		 this.descricao = descricao;
-		 this.statusAnteriores = Arrays.asList(statusAnteriores);
-	 }
-	 public String getDescricao() {
-		 return this.descricao;
-	 }
-	 
-	 public boolean naoPodeAlterarPara(StatusPedido novoStatusPedido) {
-		 return !novoStatusPedido.statusAnteriores.contains(this);
-	 }
+
+	StatusPedido(String descricao, StatusPedido... statusAnteriores) {
+		this.descricao = descricao;
+		this.statusAnteriores = Arrays.asList(statusAnteriores);
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public boolean naoPodeAlterarPara(StatusPedido novoStatusPedido) {
+		return !novoStatusPedido.statusAnteriores.contains(this);
+	}
 }
