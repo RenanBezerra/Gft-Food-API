999e090b35e4ceabaded74c80a3a57556d2b11e2
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-09-10 17:04:03 -0300

    15.3
    Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/example/demo/core/email/EmailProperties.java
@@ -0,0 +1,23 @@
+package com.example.demo.core.email;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("gftfood.email")
+public class EmailProperties {
+	
+	@NotNull
+	private String remetente;
+
+}
---
+++ src/main/java/com/example/demo/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -0,0 +1,40 @@
+package com.example.demo.infrastructure.service.email;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.core.email.EmailProperties;
+import com.example.demo.domain.service.EnvioEmailService;
+
+@Service
+public class SmtpEnvioEmailService implements EnvioEmailService {
+
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Autowired
+	private EmailProperties emailProperties;
+	
+	@Override
+	public void enviar(Mensagem mensagem) {
+		try {
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+			helper.setFrom(emailProperties.getRemetente());
+			helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
+			helper.setSubject(mensagem.getAssunto());
+			helper.setText(mensagem.getCorpo(),true);
+			
+			mailSender.send(mimeMessage);
+		} catch (Exception e) {
+			throw new EmailException("Não foi possivel enviar e-mail", e);
+		}
+		
+	}
+
+}
