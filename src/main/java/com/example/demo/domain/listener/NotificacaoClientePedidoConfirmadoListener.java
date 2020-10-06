5473ba970d38093054be6d0567ce676527c91589
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-10-06 17:59:28 -0300

    15.13
    Reagindo a Domain Events em fases especificas da transação - decidimos enviar o email só depois da transação ser executada e commitada na base

--- src/main/java/com/example/demo/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/example/demo/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,8 @@
 package com.example.demo.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.example.demo.domain.event.PedidoConfirmadoEvent;
 import com.example.demo.domain.model.Pedido;
@@ -16,18 +16,15 @@ public class NotificacaoClientePedidoConfirmadoListener {
 
 	@Autowired
 	private EnvioEmailService envioEmail;
-	
-	@EventListener
+
+	@TransactionalEventListener
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
-		
-		var mensagem = Mensagem.builder()
-				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-				.corpo("pedido-confirmado.html")
-				.variavel("pedido", pedido)
-				.destinatario(pedido.getCliente().getEmail())
+
+		var mensagem = Mensagem.builder().assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("pedido-confirmado.html").variavel("pedido", pedido).destinatario(pedido.getCliente().getEmail())
 				.build();
-		
+
 		envioEmail.enviar(mensagem);
 	}
 }
