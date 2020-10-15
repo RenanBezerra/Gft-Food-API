fd0f2fbe185cf74a77c21668adc5b5d3d3717d92
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-10-15 16:22:00 -0300

    17.6
    Adicionando outras diretivas de Cache Control na resposta HTTP

--- src/main/java/com/example/demo/api/controller/FormaPagamentoController.java
+++ src/main/java/com/example/demo/api/controller/FormaPagamentoController.java
@@ -50,7 +50,13 @@ public class FormaPagamentoController {
 		List<FormaPagamentoModel> formasPagamentoModel = formaPagamentoModelAssembler
 				.toCollectionModel(todasFormasPagamentos);
 
-		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS)).body(formasPagamentoModel);
+		return ResponseEntity.ok()
+				// .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+				// .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+				 .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+				//.cacheControl(CacheControl.noCache())
+				//.cacheControl(CacheControl.noStore())
+				.body(formasPagamentoModel);
 	}
 
 	@GetMapping("/{formaPagamentoId}")
