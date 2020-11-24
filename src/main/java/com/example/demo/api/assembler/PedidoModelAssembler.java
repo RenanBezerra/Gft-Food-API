79aa6ae5197be9b6ae45126ab56bd708e8cfa2c2
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-11-24 17:52:31 -0300

    19.20
    Refatorando construçao e inclusão de links em representation model

--- src/main/java/com/example/demo/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/example/demo/api/assembler/PedidoModelAssembler.java
@@ -5,14 +5,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.api.AlgaLinks;
 import com.example.demo.api.controller.CidadeController;
 import com.example.demo.api.controller.PedidoController;
 import com.example.demo.api.controller.RestauranteController;
@@ -22,13 +18,13 @@ import com.example.demo.api.model.PedidoModel;
 import com.example.demo.domain.model.Pedido;
 
 @Component
-public class PedidoModelAssembler
-		extends RepresentationModelAssemblerSupport<Pedido, PedidoModel>
-{
-
+public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pedido, PedidoModel> {
 
 	@Autowired
 	private ModelMapper modelMapper;
+
+	@Autowired
+	private AlgaLinks algaLinks;
 
 	public PedidoModelAssembler() {
 		super(PedidoController.class, PedidoModel.class);
@@ -38,45 +34,26 @@ public class PedidoModelAssembler
 	public PedidoModel toModel(Pedido pedido) {
 		PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
 		modelMapper.map(pedido, pedidoModel);
-	
-		TemplateVariables pageVariables = new TemplateVariables(
-				new TemplateVariable("page", VariableType.REQUEST_PARAM),
-				new TemplateVariable("size", VariableType.REQUEST_PARAM),
-				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
-		
-		TemplateVariables filtroVariables = new TemplateVariables(
-				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
-				
-				String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-				
-				pedidoModel.add(new Link(UriTemplate.of(pedidosUrl,pageVariables.concat(filtroVariables)),"pedidos"));
-		
-		
-//		pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
 
-		pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
-					.buscar(pedido.getRestaurante().getId())).withSelfRel());
-		
-		pedidoModel.getCliente().add(linkTo(methodOn(UsuarioController.class)
-				.buscar(pedido.getCliente().getId())).withSelfRel());
-		
-		pedidoModel.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
-					.buscar(pedido.getEnderecoEntrega().getCidade().getId())).withSelfRel());
-		
+		pedidoModel.add(algaLinks.linkToPedidos());
+
+		pedidoModel.getRestaurante().add(
+				linkTo(methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())).withSelfRel());
+
+		pedidoModel.getCliente()
+				.add(linkTo(methodOn(UsuarioController.class).buscar(pedido.getCliente().getId())).withSelfRel());
+
+		pedidoModel.getEnderecoEntrega().getCidade()
+				.add(linkTo(methodOn(CidadeController.class).buscar(pedido.getEnderecoEntrega().getCidade().getId()))
+						.withSelfRel());
+
 		pedidoModel.getItens().forEach(item -> {
-			item.add(linkTo(methodOn(RestauranteProdutoController.class)
-					.buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId()))
-					.withRel("produto"));
+			item.add(linkTo(methodOn(RestauranteProdutoController.class).buscar(pedidoModel.getRestaurante().getId(),
+					item.getProdutoId())).withRel("produto"));
 		});
-		
-		
+
 		return pedidoModel;
-	
-	
+
 	}
 
-	
 }
