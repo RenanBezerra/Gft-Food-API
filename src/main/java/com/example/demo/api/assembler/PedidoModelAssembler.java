1c16f3328c025af1eddc6d714d3715ad4c751053
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-11-24 16:49:44 -0300

    19.19
    adicionando template variables do filtro de pedidos

--- src/main/java/com/example/demo/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/example/demo/api/assembler/PedidoModelAssembler.java
@@ -43,10 +43,16 @@ public class PedidoModelAssembler
 				new TemplateVariable("page", VariableType.REQUEST_PARAM),
 				new TemplateVariable("size", VariableType.REQUEST_PARAM),
 				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		
+		TemplateVariables filtroVariables = new TemplateVariables(
+				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
 				
 				String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
 				
-				pedidoModel.add(new Link(UriTemplate.of(pedidosUrl,pageVariables),"pedidos"));
+				pedidoModel.add(new Link(UriTemplate.of(pedidosUrl,pageVariables.concat(filtroVariables)),"pedidos"));
 		
 		
 //		pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
