b4caae906502c420eb9d036ab121dd47d5df25a1
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-11-25 21:03:36 -0300

    19.22
    Adicionando links de transição de status de pedidos

--- src/main/java/com/example/demo/api/AlgaLinks.java
+++ src/main/java/com/example/demo/api/AlgaLinks.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.example.demo.api.controller.CidadeController;
 import com.example.demo.api.controller.CozinhaController;
 import com.example.demo.api.controller.EstadoController;
+import com.example.demo.api.controller.FluxoPedidoController;
 import com.example.demo.api.controller.FormaPagamentoController;
 import com.example.demo.api.controller.PedidoController;
 import com.example.demo.api.controller.RestauranteController;
@@ -43,6 +44,21 @@ public class AlgaLinks {
 		return new Link(UriTemplate.of(pedidosUrl, PAGINACAO_VARIABLES.concat(filtroVariables)), "pedidos");
 	}
 
+	public Link linkToConfirmacaoPedido(String codigoPedido, String rel) {
+		return linkTo(methodOn(FluxoPedidoController.class)
+				.confirmar(codigoPedido)).withRel(rel);
+	}
+	
+	public Link linkToCancelamentoPedido(String codigoPedido, String rel) {
+		return linkTo(methodOn(FluxoPedidoController.class)
+				.cancelar(codigoPedido)).withRel(rel);
+	}
+	
+	public Link linkToEntregaPedido(String codigoPedido, String rel) {
+		return linkTo(methodOn(FluxoPedidoController.class)
+				.entregar(codigoPedido)).withRel(rel);
+	}
+	
 	public Link linkToRestaurante(Long restauranteId, String rel) {
 		return linkTo(methodOn(RestauranteController.class).buscar(restauranteId)).withRel(rel);
 	}
--- src/main/java/com/example/demo/api/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/example/demo/api/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -1,5 +1,7 @@
 package com.example.demo.api.openapi.controller;
 
+import org.springframework.http.ResponseEntity;
+
 import com.example.demo.api.exceptionhandler.Problem;
 
 import io.swagger.annotations.Api;
@@ -14,19 +16,19 @@ public interface FluxoPedidoControllerOpenApi {
 	@ApiOperation("Confirmação de pedido")
 	@ApiResponses({ @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
 			@ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class) })
-	void confirmar(
+	ResponseEntity<Void> confirmar(
 			@ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
 	@ApiOperation("Cancelamento do pedido")
 	@ApiResponses({ @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
 			@ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class) })
-	void cancelar(
+	ResponseEntity<Void> cancelar(
 			@ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
 	@ApiOperation("Registrar entrega de pedido")
 	@ApiResponses({ @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
 			@ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class) })
-	void entregar(
+	ResponseEntity<Void> entregar(
 			@ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
 }
