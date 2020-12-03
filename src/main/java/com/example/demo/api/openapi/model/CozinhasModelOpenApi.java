84c7ca19a8057f390aa8d0daae42d39bd839b9d5
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-03 20:08:19 -0300

    19.41
    Corrigindo a paginação na documentação

--- src/main/java/com/example/demo/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/example/demo/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,10 +1,30 @@
 package com.example.demo.api.openapi.model;
 
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
 import com.example.demo.api.model.CozinhaModel;
 
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+
+	private CozinhasEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	private PageModelOpenApi page;
+
+	@ApiModel("CozinhasEmbeddedModel")
+	@Data
+	public class CozinhasEmbeddedModelOpenApi {
+
+		private List<CozinhaModel> cozinhas;
+	}
 
 }
