60e89bee94e3923b73c5b8de54a66ad8411a9b6b
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-12-19 20:51:40 -0300

    21.4
    Configurando o appender do Loggly no Logback

--- src/main/java/com/example/demo/core/web/WebConfig.java
+++ src/main/java/com/example/demo/core/web/WebConfig.java
@@ -2,29 +2,23 @@ package com.example.demo.core.web;
 
 import javax.servlet.Filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Autowired
-	private ApiRetirementHandler apiRetirementHandler;
-	
+//	@Autowired
+//	private ApiRetirementHandler apiRetirementHandler;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("*");
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(apiRetirementHandler);
-	}
 	@Bean
 	public Filter shalowEtagHeaderFilter() {
 		return new ShallowEtagHeaderFilter();
